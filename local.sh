c208d9335e76b1c3b056f17df3b4c23bebc4f306
Michael D Adams <michael.d.adams@gmail.com>
2024-10-25 21:07:32 -0700

    make build step optional in local.sh

--- local.sh
+++ local.sh
@@ -2,7 +2,21 @@
 
 set -e
 
-php build.php
+BUILD=1
+ARGS=()
+for ARG in "$@"; do
+	if [ "--no-build" = "$ARG" ]; then
+		BUILD=0
+	else
+		ARGS+=( "$ARG" )
+	fi
+done
+set -- "${ARGS[@]}"
+
+
+if [ $BUILD -eq 1 ]; then
+	php build.php
+fi
 
 php -S localhost:3333 -t client/
 # python3 -m http.server 3333 --directory client/
